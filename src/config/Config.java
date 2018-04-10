40311ffb5f0f2ebd04dae53f473fde5b70fb4488
Andres Yrra <yrraandres@gmail.com>
2018-04-10 16:53:17 -0700

    first commit

---
+++ src/config/Config.java
@@ -0,0 +1,20 @@
+package config;
+
+public class Config {
+
+	private String APIKey = "";
+
+	public Config(String key) {
+		this.APIKey = key;
+	}
+	
+	public String getAPIKey() {
+		return APIKey;
+	}
+
+	public void setAPIkey(String APIKey) {
+		this.APIKey = APIKey;
+	}
+	
+	
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+import config.Config;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Here we test our API calls!");
+		
+		//you can set your apikey in the Config constructor, you can later change by calling the setter
+		Config APIConfig = new Config("YOUR KEY GOES HERE");
+		
+		//now let write a method to 
+		
+	}
+
+}
